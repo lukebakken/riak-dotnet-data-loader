2cb053c5dad1c80fbe362d426fbddd72239389a6
Luke Bakken <luke@bowbak.org>
2014-09-02 18:00:41 -0700

    initial commit

---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace riak_data_loader
+{
+    using CorrugatedIron;
+
+    static class Program
+    {
+        private static readonly IRiakEndPoint cluster;
+
+        static Program()
+        {
+            cluster = RiakCluster.FromConfig("riakClusterConfiguration");
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
